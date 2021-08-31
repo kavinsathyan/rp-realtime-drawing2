2826193289e63d0f4dcbc715440cbde80f7cb447
kavinsathyan <79074982+kavinsathyan@users.noreply.github.com>
2021-08-31 09:19:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550 ,500)
+
+    canvas = createCanvas(550, 550)
+    canvas.position(570,100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+        poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded ()
+{
+    console.log('PoseNet is Initalized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+
+function draw()
+{
+    background('#FEEB75');
+}
